dd79a403aef7111e3058b16dd639cfacd4385c3c
Ramon Wang <ponmorw@gmail.com>
2024-10-08 17:58:46 -0700

    rw - updated with actual team data

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -35,13 +35,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f24-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f24-07");
+        team.addMember("Ramon");
+        team.addMember("Logan");
+        team.addMember("Eugene W");
+        team.addMember("Noah");
+        team.addMember("Divya");
+        team.addMember("Kanav");
         return team;
     }
 }
